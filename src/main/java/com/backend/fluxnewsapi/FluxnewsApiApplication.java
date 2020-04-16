fb96cdbd9113da1e38f3d3b244386aaec9da5a08
uriel <uriel.vido@hotmail.fr>
2020-04-16 21:06:58 +0200

    add prod database config

--- src/main/java/com/backend/fluxnewsapi/FluxnewsApiApplication.java
+++ src/main/java/com/backend/fluxnewsapi/FluxnewsApiApplication.java
@@ -1,5 +1,7 @@
 package com.backend.fluxnewsapi;
 
+import com.backend.fluxnewsapi.config.DbConnection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FluxnewsApiApplication {
+
+	@Autowired
+	DbConnection dbConnection;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FluxnewsApiApplication.class, args);
---
+++ src/main/java/com/backend/fluxnewsapi/config/DbConnection.java
@@ -0,0 +1,37 @@
+package com.backend.fluxnewsapi.config;
+
+import lombok.extern.flogger.Flogger;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+@Component
+public class DbConnection {
+    private static Logger logger = Logger.getLogger(DbConnection.class.getName()+":");
+
+    private static Connection getRemoteConnection() {
+        if (System.getenv("RDS_HOSTNAME") != null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                String dbName = System.getenv("RDS_DB_NAME");
+                String userName = System.getenv("RDS_USERNAME");
+                String password = System.getenv("RDS_PASSWORD");
+                String hostname = System.getenv("RDS_HOSTNAME");
+                String port = System.getenv("RDS_PORT");
+                String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+                //logger.trace("Getting remote connection with connection string from environment variables.");
+                Connection con = DriverManager.getConnection(jdbcUrl);
+                logger.info("Remote connection successful.");
+                return con;
+            }
+            catch (ClassNotFoundException e) { logger.info(e.toString());}
+            catch (SQLException e) { logger.info(e.toString());}
+        }
+        return null;
+    }
+}
