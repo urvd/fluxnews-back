65752b21441dbf68cbb8a19609f37bf1c879e2a1
uriel <uriel.vido@hotmail.fr>
2020-06-19 09:46:54 +0200

    profile adaptation

--- src/main/java/com/backend/fluxnewsapi/ServletInitializer.java
+++ src/main/java/com/backend/fluxnewsapi/ServletInitializer.java
@@ -3,7 +3,16 @@ package com.backend.fluxnewsapi;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.setInitParameter(
+				"spring.profiles.active", "dev");
+	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
